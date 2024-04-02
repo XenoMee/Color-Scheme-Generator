d6786d40d8000eb0b8bb5c33fde02c85741576df
Adrian Trandafir <trandafiradrian98@gmail.com>
2024-04-02 19:11:08 +0300

    prevent default behavior on form submit btn

--- main.js
+++ main.js
@@ -1,1 +1,34 @@
-const seedColor = 0;
+const formEl = document.querySelector('.color-header');
+const colorSchemeContainer = document.querySelector('.color-scheme');
+
+// const getColorScheme = (colorHex, colorMode) => {
+//   const colorSchemeUrl = `https://www.thecolorapi.com/scheme?hex=${colorHex}&mode=${colorMode}`;
+//   fetch(colorSchemeUrl)
+//     .then((res) => res.json())
+//     .then((data) => {
+//       colorSchemeContainer.innerHTML = '';
+
+//       const schemeColorElements = data.colors
+//         .map((color) => {
+//           colorHex = color.hex.value;
+//           return `
+//             <div class='color-element flex flex-col gap-2'>
+//                 <div class='generated-color bg-[${colorHex}]'></div>
+//                 <p class='generated-code text-center'>${colorHex}</p>
+//             </div>
+//           `;
+//         })
+//         .join('');
+
+//       colorSchemeContainer.innerHTML = schemeColorElements;
+//       colorSchemeContainer.classList.add('py-2');
+//     });
+// };
+
+formEl.addEventListener('submit', (e) => {
+  //   const seedColor = document.querySelector('.color-input').value.slice(1);
+  //   const colorSchemeMode = document.querySelector('#mode').value.toLowerCase();
+
+  e.preventDefault();
+  //   getColorScheme(seedColor, colorSchemeMode);
+});
