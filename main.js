a76b62d977d8d17cc9a3346f9615496950f554c5
Adrian Trandafir <trandafiradrian98@gmail.com>
2024-04-02 19:13:06 +0300

    generate color elements using color API inside color scheme container

--- main.js
+++ main.js
@@ -1,29 +1,29 @@
 const formEl = document.querySelector('.color-header');
 const colorSchemeContainer = document.querySelector('.color-scheme');
 
-// const getColorScheme = (colorHex, colorMode) => {
-//   const colorSchemeUrl = `https://www.thecolorapi.com/scheme?hex=${colorHex}&mode=${colorMode}`;
-//   fetch(colorSchemeUrl)
-//     .then((res) => res.json())
-//     .then((data) => {
-//       colorSchemeContainer.innerHTML = '';
+const getColorScheme = (colorHex, colorMode) => {
+  const colorSchemeUrl = `https://www.thecolorapi.com/scheme?hex=${colorHex}&mode=${colorMode}`;
+  fetch(colorSchemeUrl)
+    .then((res) => res.json())
+    .then((data) => {
+      colorSchemeContainer.innerHTML = '';
 
-//       const schemeColorElements = data.colors
-//         .map((color) => {
-//           colorHex = color.hex.value;
-//           return `
-//             <div class='color-element flex flex-col gap-2'>
-//                 <div class='generated-color bg-[${colorHex}]'></div>
-//                 <p class='generated-code text-center'>${colorHex}</p>
-//             </div>
-//           `;
-//         })
-//         .join('');
+      const schemeColorElements = data.colors
+        .map((color) => {
+          colorHex = color.hex.value;
+          return `
+            <div class='color-element flex flex-col gap-2'>
+                <div class='generated-color bg-[${colorHex}]'></div>
+                <p class='generated-code text-center'>${colorHex}</p>
+            </div>
+          `;
+        })
+        .join('');
 
-//       colorSchemeContainer.innerHTML = schemeColorElements;
-//       colorSchemeContainer.classList.add('py-2');
-//     });
-// };
+      colorSchemeContainer.innerHTML = schemeColorElements;
+      colorSchemeContainer.classList.add('py-2');
+    });
+};
 
 formEl.addEventListener('submit', (e) => {
   const seedColor = document.querySelector('.color-input').value.slice(1);
